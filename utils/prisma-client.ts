7589f1d48679f7a083b80723360cd5f75b1546e6
Harry-Crocksick <harrycrocksick1inamillion@gmail.com>
2024-02-04 16:13:37 +0630

    Prevented creating many prisma client instances unneccessarily

--- utils/prisma-client.ts
+++ utils/prisma-client.ts
@@ -1,5 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient({
-  log: ["query", "info", "warn", "error"],
-});
+const prismaClientSingleton = () => {
+  return new PrismaClient({ log: ['query', 'info', 'warn', 'error'] });
+}
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+export const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
